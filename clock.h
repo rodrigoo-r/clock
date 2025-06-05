c4303f82d6fad83b9cb1f7f2976507e0c69bd940
rodrigoo-r <2nw53j32643y@gmail.com>
2025-06-05 15:08:17 -0500

    Temporarily disable Windows implementation in clock.h for review

--- clock.h
+++ clock.h
@@ -44,7 +44,8 @@ extern "C"
 #endif
 
 #ifdef _WIN32
-#   include <windows.h>
+// Pending Windows implementation review
+// #   include <windows.h>
 #else
 #   include <time.h>
 #endif
@@ -62,11 +63,12 @@ extern "C"
 static inline long long get_nano_time()
 {
 #ifdef _WIN32
-    LARGE_INTEGER frequency, counter;
-    QueryPerformanceFrequency(&frequency);
-    QueryPerformanceCounter(&counter);
+    return 0; // Pending Windows implementation review
+    // LARGE_INTEGER frequency, counter;
+    // QueryPerformanceFrequency(&frequency);
+    // QueryPerformanceCounter(&counter);
     // Convert to nanoseconds: (counter / frequency) * 1e9
-    return (long long)((double)counter.QuadPart / frequency.QuadPart * 1e9);
+    // return (long long)((double)counter.QuadPart / frequency.QuadPart * 1e9);
 #else
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
